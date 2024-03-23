1e89a6fa44e3f7a89aa383aa1f0ffed7f419f597
Iffy Eleweke <omaleweks@gmail.com>
2024-03-22 22:16:11 -0400

    Added the Login Form component

--- Day 1/m5-w3-d1-homework/react-fb_login/src/App.js
+++ Day 1/m5-w3-d1-homework/react-fb_login/src/App.js
@@ -11,7 +11,30 @@ function App() {
 
   return (
     <div className="container">
-      
+      <Card>
+        <Card.Header>
+          <h1>My React App</h1>
+        </Card.Header>
+        <Card.Body>
+          <Card.Text>
+            <h3>Please login using one of the following:</h3>
+            {/* Login Form  */}
+            {/* FB Login Button */}
+          </Card.Text>
+        </Card.Body>
+      </Card>
     </div>
+  )
+}
+
+function LoginForm() {
+  return (
+    <form className='border mt-3 mb-5 p-3 bg-white'>
+      <label className='m-2'>Name:</label>
+      <input type="text" name="name" placeholder="Your name" />
+      <label className='m-2'>Email:</label>
+      <input type="email" name="email" placeholder="Your Email" />
+      <input type="submit" value="Login" className='btn bg-success text-white my-3' />
+    </form>
   )
 }
