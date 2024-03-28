75558d893b1ccaf29fc558889fe1dd1d40f94297
Iffy Eleweke <omaleweks@gmail.com>
2024-03-28 11:44:17 -0400

    Set the application to run on HTTPS

---
+++ Day 3/shopcart/src/checkout.js
@@ -0,0 +1,5 @@
+import React, { useState } from 'react';
+import FacebookLogin from 'react-facebook-login';
+import { Card } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
